3d973ab5c0c939d49123973537b6731c5f5749b2
Валгин <v.valgin@labirint-t.ru>
2024-04-22 16:47:08 +0300

    59.1

--- simple_1/основы/массивы/Изменить размер массива.cpp
+++ simple_1/основы/массивы/Изменить размер массива.cpp
@@ -6,6 +6,9 @@
 	1.чтобы изменить размер массива, мы должы создать новый массив с нужным кол-вом ячеек и удалить изначальный массив
 
 	2. чтобы удалить элемент, нужен новый массив с новыми ячейками
+
+
+	// ПОДУМАТЬ КАК ДОБАВИТЬ В НАЧАЛО И ВО ВТОРОЙ ЭЛЕМЕНТ МАССИВА
 */
 
 #include <iostream>
@@ -33,18 +36,45 @@ void ShowArray(int* const arr, const int size)
 // реализация функции добавления как в  STL
 // c помощью указателя на ссылку(*&arr), можно подменить адрес памяти на который он указывает
 //мы передадим адрес указателя который указывает на динамический массив
-void push_back(int *&arr, int &size, const int value)
+void push_back(int*& arr, int& size, const int value)
 {
-	
+
 	int* newArrey = new int[size + 1];
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 
 		newArrey[i] = arr[i];
 
 	}
 	newArrey[size] = value;
+	
+	size++;
+
+	delete[] arr;
+
+	arr = newArrey;
+}
+
+
+
+void pop_back(int*& arr, int& size)
+{
+	size--;
+
+	int* newArrey = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+
+		newArrey[i] = arr[i];
+
+	}
+
+
+	delete[] arr;// удаляем массив
+
+	arr = newArrey;// присваим указателю другой массив
 
 }
 
@@ -56,7 +86,13 @@ void main()
 	FillArray(arr, size);
 	ShowArray(arr, size);
 
+	push_back(arr, size, 111);
 
+	ShowArray(arr, size);
+
+	pop_back(arr, size);
+	ShowArray(arr, size);
+	
 	delete[] arr;
 
 }
